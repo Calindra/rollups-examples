d8d0e5cc2f6ae44a90bc85efecc9e48c93bd59d0
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2022-09-24 11:40:47 +0000

    baby step: send hardcoded transaction to solana inside cartesi machine

---
+++ solana-front/frontend/src/components/Greeter.tsx
@@ -0,0 +1,245 @@
+import { useWeb3React } from '@web3-react/core';
+import { JsonRpcProvider } from "@ethersproject/providers";
+
+import { Contract, ethers, Signer } from 'ethers';
+import {
+  ChangeEvent,
+  MouseEvent,
+  ReactElement,
+  useEffect,
+  useState
+} from 'react';
+import styled from 'styled-components';
+import GreeterArtifact from '../artifacts/contracts/Greeter.sol/Greeter.json';
+import { cartesiRollups } from '../utils/cartesi';
+import { Provider } from '../utils/provider';
+import { SectionDivider } from './SectionDivider';
+
+const StyledDeployContractButton = styled.button`
+  width: 180px;
+  height: 2rem;
+  border-radius: 1rem;
+  border-color: blue;
+  cursor: pointer;
+  place-self: center;
+`;
+
+const StyledGreetingDiv = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-columns: 135px 2.7fr 1fr;
+  grid-gap: 10px;
+  place-self: center;
+  align-items: center;
+`;
+
+const StyledLabel = styled.label`
+  font-weight: bold;
+`;
+
+const StyledInput = styled.input`
+  padding: 0.4rem 0.6rem;
+  line-height: 2fr;
+`;
+
+const StyledButton = styled.button`
+  width: 150px;
+  height: 2rem;
+  border-radius: 1rem;
+  border-color: blue;
+  cursor: pointer;
+`;
+
+export function Greeter(): ReactElement {
+  const context = useWeb3React<Provider>();
+  const { library, active } = context;
+
+  const [signer, setSigner] = useState<Signer>();
+  const [greeterContract, setGreeterContract] = useState<Contract>();
+  const [greeterContractAddr, setGreeterContractAddr] = useState<string>('');
+  const [greeting, setGreeting] = useState<string>('');
+  const [greetingInput, setGreetingInput] = useState<string>('');
+
+  async function sendInputToCartesiRollups() {
+    if (!signer) {
+      return;
+    }
+    try {
+      const { inputContract } = await cartesiRollups(signer);
+      const signerAddress = await inputContract.signer.getAddress();
+      console.log(`using account "${signerAddress}"`);
+
+      const payload = "AT5FiVtGESwkZI4CSYS3rB1BUKhO/SsuWkdI7U0a+EfOYhWoUFcpPgFDhCa9n6lZP4j/JurMY90/6/PY/XoErA8BAAIFaLXcC6Cywbwm74mPOjeCatSweRxlWr35eTLpIEf+WOE9c+ndk0/3nYBv5IL0AYCdTFv3mclqsrWNe8g7zMKW788smY3PSJVY8mgIeGmx7C+RnzWnx1yuebvR7LVvAwu3AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAUy4Hb7mSWFQTbYEIchzRyZVRLl4KLQEuaiG+hVkKvUa4D7xZK8QCXLSfAn7UqVg66AIgQ0PFcKgpkbDEnLEQFAQQEAQIAAzivr20fDZib7awePxMEdMwUsv7P4+23SFy4GOfkeXuwWPj1MMZHa6Xu6AMAAAAAAAAEAAAAc2x1Zw==";
+      const inputBytes = ethers.utils.toUtf8Bytes(payload);
+      // send transaction
+      const tx = await inputContract.addInput(inputBytes);
+      console.log(`transaction: ${tx.hash}`);
+      console.log("waiting for confirmation...");
+      const receipt = await tx.wait(1);
+
+      console.log(`receipt: ${JSON.stringify(receipt, null, 4)}`);
+    } catch (e) {
+      console.log('Error', e)
+    }
+  }
+
+  useEffect((): void => {
+    if (!library) {
+      setSigner(undefined);
+      return;
+    }
+
+    setSigner(library.getSigner());
+  }, [library]);
+
+  useEffect((): void => {
+    if (!greeterContract) {
+      return;
+    }
+
+    async function getGreeting(greeterContract: Contract): Promise<void> {
+      const _greeting = await greeterContract.greet();
+
+      if (_greeting !== greeting) {
+        setGreeting(_greeting);
+      }
+    }
+
+    getGreeting(greeterContract);
+  }, [greeterContract, greeting]);
+
+  function handleDeployContract(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    // only deploy the Greeter contract one time, when a signer is defined
+    if (greeterContract || !signer) {
+      return;
+    }
+
+    async function deployGreeterContract(signer: Signer): Promise<void> {
+      const Greeter = new ethers.ContractFactory(
+        GreeterArtifact.abi,
+        GreeterArtifact.bytecode,
+        signer
+      );
+
+      try {
+        const greeterContract = await Greeter.deploy('Hello, Hardhat!');
+
+        await greeterContract.deployed();
+
+        const greeting = await greeterContract.greet();
+
+        setGreeterContract(greeterContract);
+        setGreeting(greeting);
+
+        window.alert(`Greeter deployed to: ${greeterContract.address}`);
+
+        setGreeterContractAddr(greeterContract.address);
+      } catch (error: any) {
+        window.alert(
+          'Error!' + (error && error.message ? `\n\n${error.message}` : '')
+        );
+      }
+    }
+
+    deployGreeterContract(signer);
+  }
+
+  function handleGreetingChange(event: ChangeEvent<HTMLInputElement>): void {
+    event.preventDefault();
+    setGreetingInput(event.target.value);
+  }
+
+  function handleGreetingSubmit(event: MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+
+    if (!greeterContract) {
+      window.alert('Undefined greeterContract');
+      return;
+    }
+
+    if (!greetingInput) {
+      window.alert('Greeting cannot be empty');
+      return;
+    }
+
+    async function submitGreeting(greeterContract: Contract): Promise<void> {
+      try {
+        const setGreetingTxn = await greeterContract.setGreeting(greetingInput);
+
+        await setGreetingTxn.wait();
+
+        const newGreeting = await greeterContract.greet();
+        window.alert(`Success!\n\nGreeting is now: ${newGreeting}`);
+
+        if (newGreeting !== greeting) {
+          setGreeting(newGreeting);
+        }
+      } catch (error: any) {
+        window.alert(
+          'Error!' + (error && error.message ? `\n\n${error.message}` : '')
+        );
+      }
+    }
+
+    submitGreeting(greeterContract);
+  }
+
+  return (
+    <>
+      <StyledDeployContractButton
+        disabled={!active || greeterContract ? true : false}
+        style={{
+          cursor: !active || greeterContract ? 'not-allowed' : 'pointer',
+          borderColor: !active || greeterContract ? 'unset' : 'blue'
+        }}
+        onClick={handleDeployContract}
+      >
+        Deploy Greeter Contract
+      </StyledDeployContractButton>
+      <SectionDivider />
+      <StyledGreetingDiv>
+        <StyledLabel>Contract addr</StyledLabel>
+        <div>
+          {greeterContractAddr ? (
+            greeterContractAddr
+          ) : (
+            <em>{`<Contract not yet deployed>`}</em>
+          )}
+        </div>
+        {/* empty placeholder div below to provide empty first row, 3rd col div for a 2x3 grid */}
+        <div></div>
+        <StyledLabel>Current greeting</StyledLabel>
+        <div>
+          {greeting ? greeting : <em>{`<Contract not yet deployed>`}</em>}
+        </div>
+        {/* empty placeholder div below to provide empty first row, 3rd col div for a 2x3 grid */}
+        <div></div>
+        <StyledLabel htmlFor="greetingInput">Set new greeting</StyledLabel>
+        <StyledInput
+          id="greetingInput"
+          type="text"
+          placeholder={greeting ? '' : '<Contract not yet deployed>'}
+          onChange={handleGreetingChange}
+          style={{ fontStyle: greeting ? 'normal' : 'italic' }}
+        ></StyledInput>
+        <StyledButton
+          disabled={!active || !greeterContract ? true : false}
+          style={{
+            cursor: !active || !greeterContract ? 'not-allowed' : 'pointer',
+            borderColor: !active || !greeterContract ? 'unset' : 'blue'
+          }}
+          onClick={handleGreetingSubmit}
+        >
+          Submit
+        </StyledButton>
+        <StyledButton
+          onClick={sendInputToCartesiRollups}
+        >
+          Send Cartesi Input
+        </StyledButton>
+      </StyledGreetingDiv>
+    </>
+  );
+}
