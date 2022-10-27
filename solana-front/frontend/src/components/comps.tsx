63bfababe7a6c09b55300ec5d75455bda269d737
Fabio Oshiro <fabio.oshiro@gmail.com>
2022-10-27 10:56:47 -0300

    refactor: ReadTokenAccount code example

---
+++ solana-front/frontend/src/components/comps.tsx
@@ -0,0 +1,10 @@
+
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  width: 150px;
+  height: 2rem;
+  border-radius: 1rem;
+  border-color: blue;
+  cursor: pointer;
+`;
