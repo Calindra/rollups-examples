84905461d57267a7ee3dacfe9405552be16882f8
Bruno Ochotorena <hackerhilt@gmail.com>
2022-12-22 21:08:13 -0300

    chore: update dapp and cargo

--- solana-adapter/dapp-start.sh
+++ solana-adapter/dapp-start.sh
@@ -3,4 +3,4 @@ export ROLLUP_HTTP_SERVER_URL="http://127.0.0.1:5004"
 export RUST_BACKTRACE=1
 export PORTAL_ADDRESS=0xf8c694fd58360de278d5ff2276b7130bfdc0192a
 
-cargo watch -i account_info_data -x 'run'
+cargo watch -i account_info_data -i solana_smart_contract_bin -x 'run'
