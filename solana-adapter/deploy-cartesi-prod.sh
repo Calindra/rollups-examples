2eb47dc01b133bcdaa0ffbf98bf91a4bac1520e6
Fabio Oshiro <fabio.oshiro@gmail.com>
2023-01-10 12:14:36 -0300

    remove local cartesi-solana

--- solana-adapter/deploy-cartesi-prod.sh
+++ solana-adapter/deploy-cartesi-prod.sh
@@ -3,13 +3,8 @@ cd ./solana_programs_riscv
 export SOLANA_PROGRAMS_RISCV=`pwd`
 cd -
 
-cd ../../cartesi-solana
-export CARTESI_SOLANA=`pwd`
-cd -
-
 docker run \
     -v `pwd`:/workdir \
-    -v $CARTESI_SOLANA:/cartesi-solana \
     -v $SOLANA_PROGRAMS_RISCV:/solana_programs_riscv \
     -w /workdir \
     --rm \
